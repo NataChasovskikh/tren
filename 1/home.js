39eeb0ddf5db8a5f50736e5a815fbe46a3212bf0
Karlsoninit <m.norets@gmail.com>
2019-10-15 13:04:19 +0300

    change menu

--- 1/home.js
+++ 1/home.js
@@ -1,12 +1,12 @@
-var pages = new Array('one', 'two', 'three', 'four');
+let pages = new Array('one', 'two', 'three', 'four');
 
 function toggleMenu() {
  document.getElementsByClassName('wrapper')[0].classList.toggle('menu-open');
 }
 
 function goToPage(page) {
-  var wrapper = document.getElementsByClassName('wrapper')[0];
-  var sections = document.getElementsByTagName('section');
+  let wrapper = document.getElementsByClassName('wrapper')[0];
+  let sections = document.getElementsByTagName('section');
   for (i = 0; i < sections.length; i++) {
     sections[i].classList.remove('before','after');
     if (i > page) {
